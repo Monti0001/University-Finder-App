99261529afd679d00cca752591b47fa70f829c5d
Monti <144052424+Monti0001@users.noreply.github.com>
2026-07-18 15:30:58 +0530

    Refactor university.js to use base URL and proxy

--- unviersity.js
+++ unviersity.js
@@ -1,30 +1,32 @@
-let url = "http://universities.hipolabs.com/search?name=" ;
+let baseUrl = "http://universities.hipolabs.com/search?country=";
 let btn = document.querySelector("button");
-btn.addEventListener("click" , async ()=>{
-    let country = document.querySelector("input").value ;
+
+btn.addEventListener("click", async () => {
+    let country = document.querySelector("input").value;
     console.log(country);
-    let colleges = await  getcolleges(country);
+    let colleges = await getcolleges(country);
     show(colleges);
+});
 
-})
-
-function show(colleges){
+function show(colleges) {
     let list = document.querySelector("#list");
-    list.innerText = " ";
-for(col of colleges)
-{
-    console.log(col.name);
-    let li = document.createElement("li");
-    li.innerText = col.name;
-    list.appendChild(li);
+    list.innerText = "";
+    for (col of colleges) {
+        console.log(col.name);
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
 }
-}
-async function getcolleges(country){ try{
-    let res = await axios.get(url + country);
-    return res.data;
-}
-catch(e){
-    return [];
-    console.log('error - ',e);
-}
+
+async function getcolleges(country) {
+    try {
+        let targetUrl = baseUrl + country;
+        let proxyUrl = `https://api.allorigins.win/raw?url=${encodeURIComponent(targetUrl)}`;
+        let res = await axios.get(proxyUrl);
+        return res.data;
+    } catch (e) {
+        console.log('error - ', e);
+        return [];
+    }
 }
